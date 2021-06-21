6f4727f814762eb6268ac392b211c1107bd76fa3
mbalazy <p@mbalazy.pl>
2021-06-21 15:49:34 +0200

    Fix path for HeroImage

--- components/organisms/Hero/Hero.tsx
+++ components/organisms/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import heroImg from '../../../public/hero3-full.jpg'
 import { HeroWrapper, HeroImageOverlay } from './Hero.style'
 import Nav from '../../molecues/Nav/Nav'
 import HeroContentBox from '../../molecues/HeroContentBox/HeroContentBox'
@@ -9,7 +8,7 @@ const Hero = () => {
   return (
     <HeroWrapper>
       <HeroImageOverlay>
-        <Image src={heroImg} alt="kuchniass" layout="fill" objectFit="cover" />
+        <Image src={'/hero3.jpg'} alt="kuchnia" layout="fill" objectFit="cover" />
       </HeroImageOverlay>
       {/* <Nav /> */}
       <HeroContentBox />
