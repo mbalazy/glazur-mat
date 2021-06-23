3d8c890b0176f4650c8fe1b30fef692ee9da9136
mbalazy <p@mbalazy.pl>
2021-06-23 12:52:12 +0200

    Ignore ESLint in builds

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+})
