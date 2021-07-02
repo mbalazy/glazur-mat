f0a9c948296716c5d905f5a874b4f1fa63a0f755
mbalazy <p@mbalazy.pl>
2021-07-02 15:36:11 +0200

    Add Offer content

--- theme/businessInfo.ts
+++ theme/businessInfo.ts
@@ -2,6 +2,13 @@ import facebookIcon from '../public/facebook.svg'
 import ingIcon from '../public/instagram.svg'
 import whatsappIcon from '../public/whatsapp.svg'
 
+import gips_sysIcon from '../public/sys_gips.svg'
+import tilesIcon from '../public/tiles.svg'
+import plasteringIcon from '../public/plastering.svg'
+import paitingIcon from '../public/painting.svg'
+import windowIcon from '../public/window.svg'
+import floorIcon from '../public/floor.svg'
+
 const businessInfo = {
   fullTitle: 'Glazur-Mat',
   telephones: ['537 911 486'],
@@ -25,5 +32,64 @@ export const socials = [
     name: 'whatsapp',
     icon: whatsappIcon,
     link: 'https://www.whatsapp.com',
+  },
+]
+
+export const offer = [
+  {
+    name: 'Montaż Systemów Gipsowych',
+    icon: gips_sysIcon,
+    details: [
+      'Podwieszanie sufitów',
+      'Sufity wielopoziomowe z podświetleniem LED',
+      'Zabudowy nietypowe',
+      'Ścianki działowe',
+    ],
+  },
+  {
+    name: 'Kafelkowanie',
+    icon: tilesIcon,
+    details: ['Ceramika', 'Gres', 'Beton dekoracyjny', 'Cegła dekoracyjna', 'Mozaika', 'Klinkier'],
+  },
+  {
+    name: 'Gładź Tynki Posadzki',
+    icon: plasteringIcon,
+    details: [
+      'Gładzie',
+      'Suche tynki',
+      'Kamienne dywany',
+      'Posadzki żywiczne',
+      'Posadzki samopoziomujące',
+    ],
+  },
+  {
+    name: 'Malowanie Tapetowanie',
+    icon: paitingIcon,
+    details: [
+      'Malowanie farbami lateksowymi, akrylowymi, ceramicznymi',
+      'Malowanie elewacji, drewna i stali',
+      'Tapetowanie',
+      'Tynki dekoracyjne',
+      'Montaż sztukaterii',
+    ],
+  },
+  {
+    name: 'Okna i Drzwi',
+    icon: windowIcon,
+    details: [
+      'Montaż drzwi wewnętrznych i zewnętrznych',
+      'Montaż okien',
+      'Montaż drzwi przesuwnych',
+      'Montaż drzwi system ukrytych ościeżnic',
+    ],
+  },
+  {
+    name: 'Podłogi',
+    icon: floorIcon,
+    details: [
+      'Montaż paneli drewnianych oraz winylowych',
+      'Montaż desek podłogowych',
+      'Montaż cokołów i listew przypodłogowych',
+    ],
   },
 ]
