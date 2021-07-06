2c1d2058ad5e63a57a949ef7c2b6d3510f5a8770
mbalazy <p@mbalazy.pl>
2021-07-06 10:45:46 +0200

    Add Links to Nav

--- components/molecues/Nav/Nav.style.ts
+++ components/molecues/Nav/Nav.style.ts
@@ -5,6 +5,10 @@ export const NavWrapper = styled.nav`
   color: ${({ theme }) => theme.colors.white};
   background: ${({ theme }) => theme.colors.overlay};
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   position: absolute;
   top: 0;
   left: 0;
--- components/molecues/Nav/Nav.tsx
+++ components/molecues/Nav/Nav.tsx
@@ -1,10 +1,16 @@
+import React from 'react'
+import { menuItems } from '../../../theme/businessInfo'
+import BaseLink from '../../atoms/Link/Link'
 import { NavWrapper } from './Nav.style'
 
 const Nav = () => {
   return (
     <NavWrapper>
-      component
-      <p>Nav</p>
+      {menuItems.map(({ label, link }) => (
+        <BaseLink key={label} href={link}>
+          {label}
+        </BaseLink>
+      ))}
     </NavWrapper>
   )
 }
