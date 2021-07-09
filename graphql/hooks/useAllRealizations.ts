b7391df6e7a60bdcd6c977695108cb5ada662d88
mbalazy <p@mbalazy.pl>
2021-07-09 12:08:22 +0200

    Move allRealizations to custom hook

---
+++ graphql/hooks/useAllRealizations.ts
@@ -0,0 +1,8 @@
+import { getSdk } from '../../generated/graphql'
+import client from '../../lib/graphql-request'
+
+export const useAllRealizations = async () => {
+  const sdk = getSdk(client)
+  const { allRealizations } = await sdk.AllRealizations()
+  return { allRealizations }
+}
--- pages/realizacje/index.tsx
+++ pages/realizacje/index.tsx
@@ -1,19 +1,18 @@
 import Realizations from '../../components/views/Offer/Realizations/Realizations'
-import { getSdk, Realizations as RealizationsType } from '../../generated/graphql'
-import client from '../../lib/graphql-request'
+import { Realizations as RealizationType } from '../../generated/graphql'
+import { useAllRealizations } from '../../graphql/hooks/useAllRealizations'
 
 export type RealizationsProps = {
-  allRealizations: RealizationsType[]
+  allRealizations: RealizationType[]
 }
 
-const RealizationsPage = (props: RealizationsProps) => <Realizations {...props} />
-
+const RealizationsPage = (props: RealizationsProps) => {
+  return <Realizations {...props} />
+}
 export default RealizationsPage
 
 export async function getStaticProps() {
-  const sdk = getSdk(client)
-  const { allRealizations } = await sdk.AllRealizations()
-
+  const { allRealizations } = await useAllRealizations()
   return {
     props: {
       allRealizations,
