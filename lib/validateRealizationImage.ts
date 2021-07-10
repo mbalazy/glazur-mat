10f5c4cd803dcf9f44193195b6c398e9f899a1ad
mbalazy <p@mbalazy.pl>
2021-07-10 10:36:49 +0200

    Add backupImageSrc

--- lib/validateRealizationImage.ts
+++ lib/validateRealizationImage.ts
@@ -1,10 +1,11 @@
 import { IImage, Maybe } from '../generated/graphql'
 
 export const validateRealizationImage = (images?: Maybe<Maybe<IImage>[]>) => {
+  const backupImageSrc = '/lazienka1.jpg'
   if (!images) {
-    images = [{ asset: { url: '/lazienka1.jpg' } }]
+    images = [{ asset: { url: backupImageSrc } }]
   }
-  return images[0]?.asset?.url || '/lazienka1.jpg'
+  return images[0]?.asset?.url || backupImageSrc
 }
 
 export default validateRealizationImage
