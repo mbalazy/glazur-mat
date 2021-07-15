07f07adf9816de4f07bcd6c6aefcba1c2e2fc62f
mbalazy <p@mbalazy.pl>
2021-07-15 21:53:37 +0200

    Add redirect to '/' on Logo

--- components/atoms/Logo/Logo.tsx
+++ components/atoms/Logo/Logo.tsx
@@ -1,12 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
 import logo from '../../../public/glazurmat_logo.svg'
+import { useMobileMenu } from '../../../hooks/useMobileMenu'
 
 type LogoProps = {
   height?: number
   className?: string
 }
 
-const Logo = ({ height = 85, className }: LogoProps) => (
-  <img src={logo} alt="logo firmowe" height={height} className={className} />
-)
+const Logo = ({ height = 85, className }: LogoProps) => {
+  const { setOpenMobileMenu } = useMobileMenu()
+  return (
+    <Link href="/">
+      <a onClick={() => setOpenMobileMenu(false)} role="link">
+        <img src={logo} alt="logo firmowe" height={height} className={className} />
+      </a>
+    </Link>
+  )
+}
 
 export default Logo
