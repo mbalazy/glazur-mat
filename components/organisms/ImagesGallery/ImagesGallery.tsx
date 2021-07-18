e4dc897656d61dea7a242866d178b83f5618cccb
mbalazy <p@mbalazy.pl>
2021-07-18 19:37:23 +0200

    Add ClientOnlyPortal for ImagesGallery

--- components/organisms/ImagesGallery/ImagesGallery.tsx
+++ components/organisms/ImagesGallery/ImagesGallery.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import { validateRealizationProps } from '../../../lib/validateRealizationProps'
 import { ImageWrapper } from '../../molecues/RealizationThumblail/RealizationThumbnail.style'
+import ClientOnlyPortal from '../../templates/ClientOnlyPortal'
 import { GridWrapper } from '../../templates/GridWrapper.style'
 
 type ImagesGalleryProps = {
@@ -10,13 +11,18 @@ type ImagesGalleryProps = {
 
 const ImagesGallery = ({ images }: ImagesGalleryProps) => {
   return (
-    <GridWrapper>
-      {images?.map(({ id, src }) => (
-        <ImageWrapper key={id}>
-          <Image src={src as string} layout="fill" objectFit="cover" />
-        </ImageWrapper>
-      ))}
-    </GridWrapper>
+    <>
+      <GridWrapper>
+        {images?.map(({ id, src }) => (
+          <ImageWrapper key={id}>
+            <Image src={src as string} layout="fill" objectFit="cover" />
+          </ImageWrapper>
+        ))}
+      </GridWrapper>
+      <ClientOnlyPortal>
+        <p>hello from portal</p>
+      </ClientOnlyPortal>
+    </>
   )
 }
 
