22e9e98fcd53e47f01d56763b343272b9937f91d
mbalazy <p@mbalazy.pl>
2021-07-22 17:37:00 +0200

    css-modules

--- components/atoms/Stars/Stars.style.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const StarsWrapper = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 1rem;
-`
--- components/atoms/Stars/Stars.tsx
+++ components/atoms/Stars/Stars.tsx
@@ -1,5 +1,5 @@
 import starIcon from '../../../public/star.svg'
-import { StarsWrapper } from './Stars.style'
+import style from './Stars.module.scss'
 
 type StarsProps = {
   rating: number
@@ -8,11 +8,11 @@ type StarsProps = {
 const Stars = ({ rating = 5 }: StarsProps) => {
   const starsArr = Array.from(Array(rating).keys())
   return (
-    <StarsWrapper>
+    <div className={style.starsWrapper}>
       {starsArr.map((star) => (
         <img key={star} src={starIcon} alt="" />
       ))}
-    </StarsWrapper>
+    </div>
   )
 }
 
