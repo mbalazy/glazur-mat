da28548d110926303eeaf07aef63ed6d819e5705
mbalazy <p@mbalazy.pl>
2021-07-25 10:11:37 +0200

    Add arrow navigation on ImagePreview

--- components/organisms/ImagesPreview/ImagesPreview.tsx
+++ components/organisms/ImagesPreview/ImagesPreview.tsx
@@ -19,6 +19,42 @@ type ImagesPreviewProps = {
   handleClosePreview: () => void
   isPreviewOpen: boolean
 }
+import { useEffect, useState } from 'react'
+
+export function useKeyPress(targetKey: string, onPressDown = () => {}, onPressUp = () => {}) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false)
+
+  useEffect(() => {
+    // If pressed key is our target key then set to true
+    function downHandler({ key }: { key: string }) {
+      if (key === targetKey) {
+        setKeyPressed(true)
+        onPressDown()
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key }: { key: string }) => {
+      if (key === targetKey) {
+        setKeyPressed(false)
+        onPressUp()
+      }
+    }
+
+    // Add event listeners
+    window.addEventListener('keydown', downHandler)
+    window.addEventListener('keyup', upHandler)
+
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler)
+      window.removeEventListener('keyup', upHandler)
+    }
+  })
+
+  return keyPressed
+}
 
 const ImagesPreview = ({
   allImages,
@@ -41,6 +77,11 @@ const ImagesPreview = ({
       setMainImageIndex(mainImageIndex - 1)
     }
   }
+
+  useKeyPress('ArrowRight', nextImage)
+  useKeyPress('ArrowLeft', prevImage)
+  useKeyPress('l', nextImage)
+  useKeyPress('h', prevImage)
 
   return (
     <ReactModal
